697f402b20f89f7536b16cc373b5efff0e265c17
defz7 <daffa.kamal@mail.ugm.ac.id>
2024-05-13 00:31:52 +0700

    feat: implement advertisement and recenthistory sections to home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,19 @@
+import Advertisement from "./components/home/advertisement";
 import Features from "./components/home/features";
 import Header from "./components/home/header";
 import WelcomePage from "./components/home/welcoming";
+import Navbar from "./components/navbar";
+import RecentHistory from "./components/recent-history";
 
 export default function Home() {
   return (
     <main className="min-h-full w-full flex flex-col items-center">
       <div className="max-w-[389px] items-center text-sm bg-blackPrimary">
-        <WelcomePage />
         <Header />
         <Features />
+        <Advertisement />
+        <RecentHistory />
+        <Navbar />
       </div>
     </main>
   );
