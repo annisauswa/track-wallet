0a45e794dd86ff6a0ca27e80804790a26832eb6e
defz7 <daffa.kamal@mail.ugm.ac.id>
2024-05-26 23:13:18 +0700

    feat: fixed position in navbar

--- frontend/src/app/components/navbar.tsx
+++ frontend/src/app/components/navbar.tsx
@@ -21,7 +21,7 @@ export default function Navbar() {
   };
 
   return (
-    <div className="min-w-full flex flex-row items-center justify-center bottom-0 bg-white rounded-t-[40px] z-20 px-7 py-5">
+    <div className="w-[389px] fixed flex flex-row items-center justify-center bottom-0 bg-white rounded-t-[40px] z-20 px-7 py-5">
       <div className="w-full flex flex-row justify-between">
         <div className="flex space-x-9">
           <Link
